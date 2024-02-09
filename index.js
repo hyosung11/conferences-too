821803c7a6b1181427fe3b0ea4c2527ab2b01c3b
HyoSung Bidol-Lee <hyosung11@me.com>
2024-02-09 14:23:01 -0500

    Create a new datastore

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const morgan = require("morgan");
-const path = require('path'); // For joining file paths
+// const path = require('path'); // For joining file paths
 
 const app = express();
 
 // Set view engine to Pug
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
+app.set("views", "./views");
 app.set('view engine', 'pug');
 
 // Serve static files from public directory
 app.use(express.static('public'));
-app.use(morgan("common"))
+app.use(morgan("common"));
+
+// Create a new datastore
+app.use((req, res, next) => {
+  res.locals.store = new PgPersistence();
+})
 
 // Route handler for the root path
 app.get("/", (req, res) => {
